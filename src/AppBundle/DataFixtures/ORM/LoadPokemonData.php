857894a23abf642880aaa34be1ecdc0007a217ee
Robin <godartrobin@gmail.com>
2016-11-30 17:16:30 +0100

    Add pokemon fixtures

--- src/AppBundle/DataFixtures/ORM/LoadPokemonData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPokemonData.php
@@ -4,11 +4,29 @@ namespace AppBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
 class LoadPokemonData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        
+        $loader = new NativeLoader();
+        $typeSet = $loader->loadFile(__DIR__.'/../../Resources/fixtures/orm/pokemon/type.yml');
+
+        foreach ($typeSet->getObjects() as $type) {
+            $manager->persist($type);
+        }
+
+        $pokemonSet = $loader->loadFile(
+            __DIR__.'/../../Resources/fixtures/orm/pokemon/pokemon.yml',
+            $typeSet->getParameters(),
+            $typeSet->getObjects()
+        );
+
+        foreach ($pokemonSet->getObjects() as $pokemon) {
+            $manager->persist($pokemon);
+        }
+
+        $manager->flush();
     }
 }
